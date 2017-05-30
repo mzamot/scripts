3876647f66fb7f3232c41f5040e3b588fe53083f
Michael Zamot <michael.zamot@ibm.com>
2017-05-30 13:19:20 -0700

    Small fixes to code

--- geocoding.py
+++ geocoding.py
@@ -2,21 +2,18 @@ import json, requests, sys
 
 def getLatLong(address, api=""):
   url = r"https://maps.googleapis.com/maps/api/geocode/json"
-  print address
   try:
     response = requests.get(url, params={ 'address' : address, 'key' : api } )
  
     if response.status_code == 200:
       jsonBody = json.loads(response.text)
+      location = jsonBody['results'][0]['geometry']['location']
+      return { 'latitude' : location['lat'], 'longitude' : location['lng'] }
     else:
       raise Exception("Received HTTP code: %d" % response.status_code ) 
   except Exception as error:
     print error
     sys.exit(1)
-
-  location = jsonBody['results'][0]['geometry']['location']
-  return { 'latitude' : location['lat'], 'longitude' : location['lng'] }
-
 
 # Execution from command line
 # python geocoding.py address
